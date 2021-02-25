b89ca93c4b7339ffb691e514f8aca09564d17d55
Nestor <nestor.venegas95@gmail.com>
2021-02-25 17:34:43 -0600

    commit tree da30ff2956e61f7b1219eecc64fb65d83ce7ec06

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,39 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all=[]
+
+  def initialize(name, owner)
+   @name = name 
+   @owner = owner
+   @mood = "nervous"
+   self.class.all << self
+  end
+
+  def owner
+    @owner
+  end
+  
+  def owner=(new_owner)
+     @owner = new_owner
+  end
+
+  def mood=(new_mood)
+    @mood = new_mood
+ end
+
+  def self.all
+    @@all
+  end
 end
+
+
+  #Instance methods
+    #initializes with a name and an Owner (FAILED - 1)
+    #can change its owner (FAILED - 2)
+    #can't change its name (FAILED - 3)
+    #initializes with a nervous mood (FAILED - 4)
+    #can change its mood (FAILED - 5)
+  #Class methods
+    #knows all the cats (FAILED - 6)
